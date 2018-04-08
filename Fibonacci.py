2d6883b58a964d539e643b9b90370bd5f6305e61
Bharadwaj Bulusu <bbs.bharadwaj@gmail.com>
2018-04-08 17:58:46 -0400

    New script

---
+++ Fibonacci.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 17:05:33 2018
+
+@author: bbsbh
+"""
+#Normal approach
+
+n = int(input("Enter a number: "))
+a = 0
+b = 1   
+for i in range(n):        
+    a,b = b,a+b  
+    print (a)
+        
+#Using Recursion
+n = int(input("Enter a number: "))
+
+def fib(n):
+ if n==0:
+     return 0
+ elif n==1:
+     return 1
+ else:
+     return fib(n-1)+fib(n-2)
+
+print (fib(n))
+
+#Using a range of numbers
+
+startnum = int(input("Enter a start number: "))
+endnum = int(input("Enter an end number: "))
+def fib(n):
+ if n==0:
+     return 0
+ elif n==1:
+     return 1
+ else:
+     return fib(n-1)+fib(n-2)
+
+for i in range(startnum, endnum):
+    print (fib(i))
+    
---
+++ map_lamba.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 14:45:57 2018
+
+@author: bbsbh
+"""
+print (list(map(len, ["Hi", "Bharadwaj", "Bulusu"])))
+
+#Using Lambda function
+cube = lambda x: x**3
+print (list(map(cube, [4,10,23])))
+
