971432262a9ae1e47eecf0542c0b7e41d2be7ae8
Bharadwaj Bulusu <bbs.bharadwaj@gmail.com>
2018-05-31 21:31:20 -0400

    Adding file

---
+++ List_Comprehensions.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+x = [x * x for x in range(10)]
+print(x)
+
+
+x = [x for x in input("Enter string: ").upper()]
+print ("".join(x))
+
+
+d = {x: x*x for x in range(10)}
+print (d)
+
+###################################
+
+lis = [1,2,8,8,2,4,5]
+print (set(lis))
+
+#############################
+s = 0
+for x in lis:
+    s += x
+print (s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
